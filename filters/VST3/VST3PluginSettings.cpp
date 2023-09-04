7dd58d7fe9376a17e86b47efd806afeea9e994ca
xcv <vzhegv.kirill@rambler.ru>
2023-09-04 13:56:04 +0500

    VST3: migrate VST3PluginSettings class

---
+++ filters/VST3/VST3PluginSettings.cpp
@@ -0,0 +1,132 @@
+﻿/***
+ *     ▄████▄  ▒██   ██▒ ██▒   █▓ █    ██   ██████ ▓█████  ██▀███
+ *    ▒██▀ ▀█  ▒▒ █ █ ▒░▓██░   █▒ ██  ▓██▒▒██    ▒ ▓█   ▀ ▓██ ▒ ██▒
+ *    ▒▓█    ▄ ░░  █   ░ ▓██  █▒░▓██  ▒██░░ ▓██▄   ▒███   ▓██ ░▄█ ▒
+ *    ▒▓▓▄ ▄██▒ ░ █ █ ▒   ▒██ █░░▓▓█  ░██░  ▒   ██▒▒▓█  ▄ ▒██▀▀█▄
+ *    ▒ ▓███▀ ░▒██▒ ▒██▒   ▒▀█░  ▒▒█████▓ ▒██████▒▒░▒████▒░██▓ ▒██▒
+ *    ░ ░▒ ▒  ░▒▒ ░ ░▓ ░   ░ ▐░  ░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░░░ ▒░ ░░ ▒▓ ░▒▓░
+ *      ░  ▒   ░░   ░▒ ░   ░ ░░  ░░▒░ ░ ░ ░ ░▒  ░ ░ ░ ░  ░  ░▒ ░ ▒░
+ *    ░         ░    ░       ░░   ░░░ ░ ░ ░  ░  ░     ░     ░░   ░
+ *    ░ ░       ░    ░        ░     ░           ░     ░  ░   ░
+ *    ░                      ░
+ *
+ *
+ *	 VST3 pkugin wrapper
+ *	 15 fev 2020
+ */
+
+#include "stdafx.h"
+#include "VST3PluginSettings.h"
+
+tresult PLUGIN_API VST3PluginSettings::read(void* buffer, int32 numBytes, int32* numBytesRead)
+{
+	if (buffer == NULL && numBytes == NULL)
+		goto LEAVE_;
+	if (!m_buf)
+		goto LEAVE_;
+	if (m_sk > numBytes)
+		goto LEAVE_;
+	if (numBytes > m_s)
+		numBytes -= (numBytes - m_s);
+
+	__try {
+		memcpy(buffer, &m_buf[m_sk], numBytes);
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER) {
+		goto LEAVE_;
+	}
+	m_sk += numBytes;
+
+	__try {
+		if (numBytesRead)
+			*numBytesRead = numBytes;
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER) {
+		goto LEAVE_;
+	}
+
+	return kResultTrue;
+
+LEAVE_:
+	return kResultFalse;
+}
+
+tresult PLUGIN_API VST3PluginSettings::write(void* buffer, int32 numBytes, int32* numBytesWritten) {
+	if (buffer == NULL && numBytes == NULL)
+		goto LEAVE_;
+
+	m_buf = (char*)realloc(m_buf, numBytes + m_s);
+	if (m_buf == NULL)
+		goto LEAVE_;
+	__try {
+		memcpy(m_buf + m_sk, buffer, numBytes);
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER) {
+		goto LEAVE_;
+	}
+
+	m_s += numBytes;
+	m_sk += numBytes;
+	__try {
+		if (numBytesWritten)
+			*numBytesWritten = numBytes;
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER) {
+		goto LEAVE_;
+	}
+	return kResultTrue;
+
+LEAVE_:
+	return kResultFalse;
+}
+
+tresult PLUGIN_API VST3PluginSettings::seek(int64 pos, int32 mode, int64* result) {
+	if (mode == kIBSeekSet)
+		m_sk = pos;
+	else if (mode == kIBSeekCur)
+		m_sk += pos;
+	else if (mode == kIBSeekEnd)
+		m_sk = m_s;
+
+	__try {
+		if (result != 0)
+			*result = m_sk;
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER) {
+		return kResultFalse;
+	}
+	return kResultTrue;
+}
+
+tresult PLUGIN_API VST3PluginSettings::tell(int64* pos) {
+	if (pos != 0) {
+		__try {
+			*pos = m_sk;
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER) {
+			goto LEAVE_;
+		}
+		return kResultTrue;
+	}
+LEAVE_:
+	return kResultFalse;
+}
+
+tresult PLUGIN_API VST3PluginSettings::getStreamSize(int64& size) {
+	if (&size != 0) {
+		__try {
+			size = m_s;
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER) {
+			goto LEAVE_;
+		}
+		return kResultTrue;
+	}
+LEAVE_:
+	return kResultFalse;
+}
+
+tresult PLUGIN_API VST3PluginSettings::setStreamSize(int64 size) {
+	m_s = size;
+	return kResultTrue;
+};
---
+++ filters/VST3/VST3PluginSettings.h
@@ -0,0 +1,72 @@
+﻿/***
+ *     ▄████▄  ▒██   ██▒ ██▒   █▓ █    ██   ██████ ▓█████  ██▀███
+ *    ▒██▀ ▀█  ▒▒ █ █ ▒░▓██░   █▒ ██  ▓██▒▒██    ▒ ▓█   ▀ ▓██ ▒ ██▒
+ *    ▒▓█    ▄ ░░  █   ░ ▓██  █▒░▓██  ▒██░░ ▓██▄   ▒███   ▓██ ░▄█ ▒
+ *    ▒▓▓▄ ▄██▒ ░ █ █ ▒   ▒██ █░░▓▓█  ░██░  ▒   ██▒▒▓█  ▄ ▒██▀▀█▄
+ *    ▒ ▓███▀ ░▒██▒ ▒██▒   ▒▀█░  ▒▒█████▓ ▒██████▒▒░▒████▒░██▓ ▒██▒
+ *    ░ ░▒ ▒  ░▒▒ ░ ░▓ ░   ░ ▐░  ░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░░░ ▒░ ░░ ▒▓ ░▒▓░
+ *      ░  ▒   ░░   ░▒ ░   ░ ░░  ░░▒░ ░ ░ ░ ░▒  ░ ░ ░ ░  ░  ░▒ ░ ▒░
+ *    ░         ░    ░       ░░   ░░░ ░ ░ ░  ░  ░     ░     ░░   ░
+ *    ░ ░       ░    ░        ░     ░           ░     ░  ░   ░
+ *    ░                      ░
+ *
+ *
+ *	 VST3 pkugin wrapper
+ *	 15 fev 2020
+ */
+
+#pragma once
+
+#include <memory>
+#include "pluginterfaces/vst/vsttypes.h"
+#include "pluginterfaces/base/ibstream.h"
+
+using namespace Steinberg;
+using namespace Steinberg::Vst;
+
+class VST3PluginSettings : public IBStream, ISizeableStream
+{
+public:
+
+	VST3PluginSettings() {};
+	~VST3PluginSettings() {
+		if (m_buf != 0)
+			free(m_buf);
+	};
+
+	virtual tresult PLUGIN_API queryInterface(const TUID _iid, void** obj) override {
+		QUERY_INTERFACE(_iid, obj, FUnknown::iid, IBStream)
+			QUERY_INTERFACE(_iid, obj, IBStream::iid, IBStream)
+			QUERY_INTERFACE(_iid, obj, ISizeableStream::iid, ISizeableStream)
+
+			* obj = nullptr;
+		return kResultFalse;
+	};
+
+	virtual uint32 PLUGIN_API addRef() override {
+		return 1;
+	};
+
+	virtual uint32 PLUGIN_API release() override {
+		return 1;
+	};
+
+	//Read bytes from stream buffer
+	virtual tresult PLUGIN_API read(void* buffer, int32 numBytes, int32* numBytesRead = 0) override;
+
+	//Write to stream buffer
+	virtual tresult PLUGIN_API write(void* buffer, int32 numBytes, int32* numBytesWritten = 0) override;
+
+	//Set current position in the stream
+	virtual tresult PLUGIN_API seek(int64 pos, int32 mode, int64* result = 0) override;
+	virtual tresult PLUGIN_API tell(int64* pos) override;
+
+	//ISizeableStream
+	virtual tresult PLUGIN_API getStreamSize(int64& size) override;
+	virtual tresult PLUGIN_API setStreamSize(int64 size) override;
+
+private:
+	char* m_buf = 0;
+	int64 m_sk = 0;
+	int64 m_s = 0;
+};
